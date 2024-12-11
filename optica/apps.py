d2878bcc97f71e1e925dd1dfe73f1305a9f6fcd0
Derek Needham A. <94086268+derekillo@users.noreply.github.com>
2024-12-11 12:47:59 -0300

    Delete optica directory

--- optica/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpticaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'optica'
-
-    def ready(self):
-        import optica.signals
--- optica/middleware.py
+++
@@ -1,8 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-class NoCacheMiddleware(MiddlewareMixin):
-    def process_response(self, request, response):
-        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-        response['Pragma'] = 'no-cache'
-        response['Expires'] = '0'
-        return response
