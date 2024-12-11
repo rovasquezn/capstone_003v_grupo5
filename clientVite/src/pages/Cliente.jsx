08d4c06cee5f118e5e2336146248e9de7a5a9203
Derek Needham A. <94086268+derekillo@users.noreply.github.com>
2024-12-11 12:47:16 -0300

    Delete clientVite directory

--- clientVite/src/pages/Cliente.jsx
+++
@@ -1,5 +0,0 @@
-//import { TasksList } from "../components/TasksList";
-
-export function Cliente() {
-  return <div>clientes</div>;
-}
--- clientVite/src/pages/ClienteNuevo.jsx
+++
@@ -1,5 +0,0 @@
-//import { TasksList } from "../components/TasksList";
-
-export function ClienteNuevo() {
-  return <div>Cliente_Nuevo</div>;
-}
